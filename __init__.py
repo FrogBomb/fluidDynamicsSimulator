84e4d3c963bc38c048003729098d1b7e0ca7ef13
FrogBomb <TLBlanchet@gmail.com>
2017-07-02 01:32:00 -0600

    rename some functions
    ok!

--- __init__.py
+++ __init__.py
@@ -10,12 +10,12 @@ import cv2
 from fluidDoubleCone import FluidDoubleCone, RGB
 from fluidDoubleConeView import FluidDCViewCtrl
 
-def example2():
+def slow_example():
     inImg = cv2.imread("gameFace.jpg")
     game_face_dCone = FluidDoubleCone(inImg, RGB)
     ui = FluidDCViewCtrl(game_face_dCone, 615, 737)
 
-def example1():
+def fast_example():
     inImg = cv2.imread("fastKid.png")
     fast_kid_dCone = FluidDoubleCone(inImg, RGB)
     ui = FluidDCViewCtrl(fast_kid_dCone, 200*2, 200*2)
@@ -23,4 +23,4 @@ def example1():
     ui.mainloop()
 
 if __name__ == "__main__":
-    example1()
+    fast_example()
